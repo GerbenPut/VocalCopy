b841ed68f2fcddccf29fec6a02d1740ae94fe4cc
Gerben Put <gerbenputprivate@gmail.com>
2022-05-12 23:52:07 +0200

    updated website

--- VocalCopy/views.py
+++ VocalCopy/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.core.files.storage import FileSystemStorage
+
 #python scripts
 from VocalCopy.backend import test
 from .backend.test import TestFunction
 
-
 def index(request):
-    TestFunction()
+    # TestFunction()
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
     return render(request, "index.html")
