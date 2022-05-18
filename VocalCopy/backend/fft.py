e450388377a0a4732739ce95f8709f26a72a11a9
Mattias <mattiasheijboer@yahoo.com>
2022-05-18 14:57:50 +0200

    Add FFT to website

---
+++ VocalCopy/backend/fft.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.io import wavfile
+from pylab import *
+
+def fastFourierTransform(filename):
+    fs, data = wavfile.read(filename)
+    print(".wav file loaded")
+    #(TODO: automatically mix stereo file into mono)
+    a = data.T[0] # pick the first channel in a stereo file
+    print("Normalising track...")
+    peak = max(abs(a))
+    b=[(ele*32767//peak) for ele in a]
+    print("Track normalised")
+    time = arange(0, len(a)/fs, 1/fs)
+    plt.subplots(2, 2, figsize=(16,9))
+    plt.subplot(2,2,1)
+    plt.ylim([-32767, 32767])
+    plt.xlim([0, len(a)/fs])
+    plt.yticks(arange(-2**15, 2**15+1, 2**13))
+    plt.plot(time, b)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude')
+    c = fft(b) # fast fourier transform
+    print("Fourier transform successful")
+    d = len(c)//2  # you only need half of the fft list (real signal symmetry)
+    e = abs(c[:(d-1)])
+    k = arange(len(data)//2-1)
+    T = len(k)/fs  # where fs is the sampling frequency
+    plt.subplot(2, 2, 2)
+    plt.plot(e, k/T, 'r')
+    plt.ylim([20,20000])
+    plt.xlim([0, max(e)]) 
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Magnitude')
+    plt.subplot(2, 2, 3)
+    plt.specgram(b, Fs=fs)
+    plt.savefig('static/images/output.png')
--- VocalCopy/views.py
+++ VocalCopy/views.py
@@ -1,11 +1,12 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.core.files.storage import FileSystemStorage
 
 #python scripts
-from VocalCopy.backend import test
-from .backend.test import TestFunction
+from VocalCopy.backend import fft
+from .backend.fft import fastFourierTransform
 
 def index(request):
     # TestFunction()
@@ -13,4 +14,6 @@ def index(request):
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
+        fastFourierTransform(f'media/{uploaded_file.name}')
+        os.remove(f'media/{uploaded_file.name}')
     return render(request, "index.html")
